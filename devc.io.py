9628e66d94016bf0ae010ce7bdfbcfc4f2ffea6f
sedevc <info@devc.io>
2015-04-05 22:57:05 +0200

    some changes

--- devc.io.py
+++ devc.io.py
@@ -1,32 +1,40 @@
-from flask import Flask, abort, url_for, render_template, request
-
+from flask import Flask, abort, url_for, render_template, request, flash, redirect
+from forms import LoginForm
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 
 @app.route('/')
 def index():
-    return render_template('tpl.html')
+    pagename = "Start"
+    return render_template('tpl.html', pagename=pagename)
 
 @app.route('/about')
 def about():
-    return render_template('tpl.html')
+    pagename = "Om"
+    return render_template('tpl.html', pagename=pagename)
 
 @app.route('/cv')
 def cv():
-    return render_template('tpl.html')
+    pagename = "CV"
+    return render_template('tpl.html', pagename=pagename)
 
 @app.route('/contact')
 def contact():
-    return render_template('tpl.html')
+    pagename = "Kontakt"
+    return render_template('tpl.html', pagename=pagename)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    abort(401)
-    if request.method == 'POST':
-        return render_template('tpl.html')
-    else:
-        return render_template('tpl.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form)
 
 
 if __name__ == '__main__':
